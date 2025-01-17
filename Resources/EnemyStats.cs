f8e602ecc138436c7dc2cc03d36b4cd95d6cd52d
rnakkas <rahman.nakkas@gmail.com>
2025-01-17 20:58:04 +1100

    started creating ranged enemy

---
+++ Resources/EnemyStats.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+namespace ElGatoProject.Resources;
+
+[GlobalClass]
+public partial class EnemyStats : Resource
+{
+    public enum Type
+    {
+        MeleeEnemyStationary,
+        MeleeEnemyMoving,
+        RangedEnemyLight,
+        RangedEnemyHeavy
+    }
+    
+    [Export] public Type EnemyType { get; set; }
+    [Export] public int EnemyHealth { get; set; }
+    [Export] public int AttackDamage;
+    [Export] public float Knockback;
+    [Export] public float PatrolSpeed;
+    [Export] public float ChaseSpeed;
+    [Export] public float ChaseTime;
+    [Export] public float HurtStaggerTime;
+}
