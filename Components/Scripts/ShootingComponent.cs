061acc2a54312c9abee39a714177e2cff4a907d8
rnakkas <rahman.nakkas@gmail.com>
2025-01-31 16:40:47 +1100

    started working on shooting component

--- Components/Scripts/ShootingComponent.cs
+++ Components/Scripts/ShootingComponent.cs
@@ -6,5 +6,20 @@ namespace ElGatoProject.Components.Scripts;
 [GlobalClass]
 public partial class ShootingComponent : Node2D
 {
-	
+	[Export] private float _shootingCooldownTime;
+	[Export] private int _bulletDamage;
+	[Export] private float _bulletKnockback;
+	[Export] private float _bulletsPerShot;
+	[Export] private float _bulletSwayAngle;
+	[Export] private float _bulletSpeed;
+	[Export] private float _bulletLifeTime;
+	[Export] private float _rapitFireTime;
+	[Export] private PackedScene _bulletScene;
+	[Export] private Marker2D _muzzle;
 }
+
+/*
+ * Shooting compoent
+ * Sets the bullet properties
+ * Can choose what type of bullet to shoot
+ */
